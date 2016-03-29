df16f3ab6ac86937c558c182a69efc7ababd39f1
thalesmiguel <thales.miguel@gmail.com>
2016-03-29 14:25:06 -0300

    v4

--- app/assets/javascripts/slips.js
+++ app/assets/javascripts/slips.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
   
+  $(document).ready(function () {
+    $('textarea[data-limit-rows=true]').on('keypress', function (event) {
+      var textarea = $(this),
+          numberOfLines = (textarea.val().match(/\n/g) || []).length + 1,
+          maxRows = parseInt(textarea.attr('rows'));
+      
+      if (event.which === 13 && numberOfLines === maxRows ) {
+        return false;
+      }
+    });
+  });
+  
   var printArray = [];
   
   $("#slip_value").maskMoney({thousands:'.', decimal:','});
--- app/models/slip.rb
+++ app/models/slip.rb
@@ -1,7 +1,3 @@
 class Slip < ActiveRecord::Base
-  
-  # validates :value, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
   validates_presence_of :value, :from, :city, :date, :signature
-  # monetize :value
-
 end
